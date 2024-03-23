0d183bcba4aa03efb0e687f79747c8248b1479e0
sajidjawaid <154744043+sajidjawaid@users.noreply.github.com>
2024-03-23 05:55:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+// q no 1 
+// let numRows = 3; 
+// let multidimensionalArray = [];
+// for (let i = 0; i < numRows; i++) {
+//     multidimensionalArray.push([]);
+// }
+// console.log(multidimensionalArray);
+
+// q no 1 end
+
+
+
+
+// q no 3 
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+//  end q no 3
+
+
+// q no 4
+
+// for (var i = 1; i <= 15; i++) {
+//     document.write("2*" + i + "=" + 2 * i + "<br>");
+// }
+
+// var tableNumber = +prompt("Enter the table number (up to 15):");
+
+// q no 4 end
+
+
+
+// q no 5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// q no 5 end
+
+
+
+// q no 6
+
+// for (var i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// for (var i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// for (var i = 0; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// for (var i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+// for (var i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+
+// q no 6 end
